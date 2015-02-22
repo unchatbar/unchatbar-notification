df5f0478e9ab73df7f9b21e7c9ffefb4e1206bd2
gernsdorfer <larswiedemann@gmx.net>
2015-02-22 22:40:36 +0100

    fix stream sound

--- app/scripts/run.js
+++ app/scripts/run.js
@@ -12,15 +12,14 @@ angular.module('unchatbar-notification').run(['$rootScope','Notify',
         Notify._initStreamSound();
         Notify._initMessageSound();
         Notify._getNotificationPermission();
-        $rootScope.$on('StreamUpdate', function (event, streamsWaitingForYourAnswer) {
-            if (streamsWaitingForYourAnswer.length > 0) {
+        $rootScope.$on('StreamUpdate', function (event, data) {
+            if (data.waitingClients.length > 0) {
                 Notify.streamCallStart();
             } else {
                 Notify.streamCallStop();
             }
         });
         $rootScope.$on('MessageUpdateUnreadMessage', function (event, message) {
-            //document.hidden = true;
             Notify.textMessage(message);
         });
 
